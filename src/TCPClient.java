86b03fba27d0731c5a40bfa46322c3d547529fe2
Shaikh Islam <shaikhalvee@gmail.com>
2025-02-22 02:20:41 -0600

    TCP server and client added

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.net.Socket;
 
 public class TCPClient {
+
+	static String fileName = "my_image.jpg";
+
+	public static void main(String[] args) {
+		try (Socket socket = new Socket(Constants.IP.LOCALHOST, Constants.Ports.SERVER_PORT)) {
+			PrintWriter outputWriter = new PrintWriter(socket.getOutputStream(), true);
+			outputWriter.println(fileName);
+
+			// Read server response
+			BufferedReader inputFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			// Expect "FILENAME:<renamed>" or "ERROR:..."
+			String line = inputFromServer.readLine();
+			if (line.startsWith("ERROR")) {
+				System.out.println(line);
+				return;
+			}
+
+			// Parse renamed filename
+			String renamed = line.split(":", 2)[1];
+			// Next line: "FILESIZE:<size>"
+			line = inputFromServer.readLine();
+			long fileSize = 0;
+			if (line.startsWith("FILESIZE:")) {
+				fileSize = Long.parseLong(line.split(":", 2)[1]);
+			}
+
+			// Receive file
+			File outputFile = new File(Constants.FilePath.CLIENT_IMAGE_FOLDER, renamed);
+			FileOutputStream fos = new FileOutputStream(outputFile);
+			InputStream socketInputFromServer = socket.getInputStream();
+			byte[] buffer = new byte[4096];
+			int totalRead = 0;
+			int bytesRead;
+			while (totalRead < fileSize && (bytesRead = socketInputFromServer.read(buffer)) != -1) {
+				fos.write(buffer, 0, bytesRead);
+				totalRead += bytesRead;
+			}
+			fos.close();
+			System.out.println("File received and saved as: " + outputFile.getName() + " in path" + outputFile.getAbsolutePath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,4 +1,89 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class TCPServer {
+
+	final static int BUFFER_SIZE = 4096;
+
+	public static void main(String[] args) {
+		try(ServerSocket serverSocket = new ServerSocket(Constants.Ports.SERVER_PORT)) {
+			System.out.println("[Server] TCP Server started on port " + Constants.Ports.SERVER_PORT + "...");
+
+			while (true) {
+				// Accept one client (single-threaded)
+				Socket clientSocket = serverSocket.accept();
+				System.out.println("[Server] Accepted a client: " + clientSocket.getRemoteSocketAddress() +
+						"with inet address: " + clientSocket.getInetAddress());
+
+				// Create input/output streams
+				BufferedReader inputReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				PrintWriter outputWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+
+				// Read a request line from the client
+				String clientInput = inputReader.readLine();
+				System.out.println("[Server] Client request: " + clientInput);
+
+				// "Rename" logic: for demonstration, we’ll just append
+				// the client’s remote port to the file name
+				String renamedFile = appendInfoToFileName(clientInput, clientSocket.getPort());
+
+				// Send the image
+				File file = new File(Constants.FilePath.SERVER_IMAGE_FOLDER, clientInput);
+				if (!file.exists()) {
+					// Simple error handling: notify client
+					outputWriter.println("ERROR: File not found");
+					clientSocket.close();
+					continue;
+				}
+
+				sendFile(file, renamedFile, clientSocket);
+
+				clientSocket.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void sendFile(File file, String renamedFile, Socket clientSocket) {
+		try {
+			// First send the "renamed" filename
+			PrintWriter outputWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+			outputWriter.println("FILENAME:" + renamedFile);
+
+			// Then send file size
+			long fileSize = file.length();
+			outputWriter.println("FILESIZE:" + fileSize);
+
+			// Send file contents (binary)
+			BufferedInputStream fileIn = new BufferedInputStream(new FileInputStream(file));
+			OutputStream socketOut = clientSocket.getOutputStream();
+
+			byte[] buffer = new byte[BUFFER_SIZE];
+			int bytesRead;
+			while ((bytesRead = fileIn.read(buffer)) != -1) {
+				socketOut.write(buffer, 0, bytesRead);
+			}
+
+			socketOut.flush();
+			fileIn.close();
+			System.out.println("[Server] File sent to client with renamed: " + renamedFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String appendInfoToFileName(String requestedFile, int port) {
+		// E.g., "picture.jpg" -> "picture_59128.jpg"
+		int dotIndex = requestedFile.lastIndexOf('.');
+		if (dotIndex == -1) {
+			return requestedFile + "_" + port;
+		} else {
+			String name = requestedFile.substring(0, dotIndex);
+			String ext = requestedFile.substring(dotIndex);
+			return name + "_" + port + ext;
+		}
+	}
 }
