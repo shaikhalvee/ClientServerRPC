aff639dba513a2a734a7ca3c9fa095f82e1417c1
Shaikh Islam <shaikhalvee@gmail.com>
2025-02-21 20:20:48 -0600

    constants added

---
+++ src/Constants.java
@@ -0,0 +1,16 @@
+public interface Constants {
+	public interface Modes {
+		String SERVER = "server";
+		String CLIENT = "client";
+	}
+	public interface Modules {
+		String TCP = "tcp";
+		String UDP = "udp";
+		String MTCP = "mtcp";
+		String SRPC = "srpc";
+		String ARPC = "arpc";
+	}
+	public interface Ports {
+		String LOCAL_PORT = "8080";
+	}
+}
