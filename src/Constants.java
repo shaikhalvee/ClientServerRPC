99b3576e6dd1ebf7f3163cc9b861110faf8e6dc0
Shaikh Islam <shaikhalvee@gmail.com>
2025-02-22 02:20:15 -0600

    constants for project added

--- src/Constants.java
+++ src/Constants.java
@@ -1,16 +1,27 @@
 public interface Constants {
-	public interface Modes {
+	interface Modes {
 		String SERVER = "server";
 		String CLIENT = "client";
 	}
-	public interface Modules {
+	interface Modules {
 		String TCP = "tcp";
 		String UDP = "udp";
 		String MTCP = "mtcp";
 		String SRPC = "srpc";
 		String ARPC = "arpc";
 	}
-	public interface Ports {
-		String LOCAL_PORT = "8080";
+	interface IP {
+		String SERVER_IP = "192.168.5.123";
+		String CLIENT_IP = "127.0.0.1";
+		String LOCALHOST = "127.0.0.1";
+	}
+	interface Ports {
+		int LOCAL_PORT = 8080;
+		int SERVER_PORT = 8088;
+	}
+
+	interface FilePath {
+		String SERVER_IMAGE_FOLDER = "./image/server";
+		String CLIENT_IMAGE_FOLDER = "./image/client";
 	}
 }
